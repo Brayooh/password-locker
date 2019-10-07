7f2b8435d39e00b5454ee8266c5bb3d92f6b3c95
brayooh <brianndichu.bn@gmail.com>
2019-10-07 11:21:57 +0300

    created _init_ and listed properties

--- locker.py
+++ locker.py
@@ -13,15 +13,15 @@ class User:
     # global users_list
     users_list = []
 
-#     def __init__(self, first_name, last_name, password):
-#         '''
-#         Method to define the properties for each user object will hold.
-#         '''
+    def __init__(self, first_name, last_name, password):
+        '''
+        Method to define the properties for each user object will hold.
+        '''
 
-#         # instance variables
-#         self.first_name = first_name
-#         self.last_name = last_name
-#         self.password = password
+        # instance variables
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
 
 #     def save_user(self):
 #         '''
